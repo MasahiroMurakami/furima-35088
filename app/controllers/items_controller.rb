b765073007d30c3df85ed0070a65ed7980105a2a
MasahiroMurakami <kodama.p19870112@gmail.com>
2021-04-09 19:25:37 +0900

    Rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
-  #before_action :authenticate_user!, except: [:index, ]
+  # before_action :authenticate_user!, except: [:index, ]
 
   def index
-    
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,25 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   before do
     @user = FactoryBot.build(:user)
   end
   describe 'ユーザー新規登録' do
-    context  '新規登録できるとき' do
-      it "全ての項目の入力が存在すれば登録できること" do 
+    context '新規登録できるとき' do
+      it '全ての項目の入力が存在すれば登録できること' do
         expect(@user).to be_valid
       end
 
-      it "passwordが6文字以上であれば登録できること" do
+      it 'passwordが6文字以上であれば登録できること' do
         @user.password
         @user.password_confirmation
         expect(@user).to be_valid
       end
     end
 
-    context  '新規登録できないとき' do
-      it "nicknameが空の場合は登録できないこと" do 
+    context '新規登録できないとき' do
+      it 'nicknameが空の場合は登録できないこと' do
         @user.nickname = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Nickname can't be blank")
@@ -44,11 +43,11 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
 
-      it "passwordが5文字以下であれば登録できないこと" do
+      it 'passwordが5文字以下であれば登録できないこと' do
         @user.password = '1234a'
         @user.password_confirmation = '1234a'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
 
       it 'passwordとpassword_confirmationが不一致では登録できないこと' do
