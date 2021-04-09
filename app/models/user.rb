faf407cc82e3bb44928d6b652fa320a42df0eaf9
MasahiroMurakami <kodama.p19870112@gmail.com>
2021-04-09 21:29:14 +0900

    ユーザー管理機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,               presence: true
-  validates :encrypted_password,     presence: true, length: { minimum: 6 }
-  validates :family_name,            presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :family_name_kana,       presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name,             presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name_kana,        presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth_day,              presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :encrypted_password, length: { minimum: 6 }
+    validates :family_name,        format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :family_name_kana,   format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name,         format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name_kana,    format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birth_day
+  end
 
   has_many :buyer_histories
   has_many :items, dependent: :destroy
