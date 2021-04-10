d55002ee1315d29c9a069c8b6063b1d3a008277f
MasahiroMurakami <kodama.p19870112@gmail.com>
2021-04-10 21:21:46 +0900

    history削除

--- .history/config/routes_20210408180817.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root to: "items#index"
-  get 'items/index'
-  resources :prototypes, only: [:index]
-end
--- .history/config/routes_20210408190630.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root to: "items#index"
-  #get 'items/index'
-  #resources :items, only: [:index]
-end
