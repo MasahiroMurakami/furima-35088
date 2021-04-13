bfa482369b023f141bdcf6b18c7d94e1be919ed9
MasahiroMurakami <kodama.p19870112@gmail.com>
2021-04-13 12:43:29 +0900

    itemモデルの単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :info
-    validates :price,numericality:{ only_integer: true, greater_than: 299, less_than: 10000000 }
+    validates :price,format:{ with: /\A[0-9]+\z/ },numericality:{ only_integer:true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     with_options numericality: { other_than: 1 } do
       validates :category_id
       validates :sales_status_id
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    image                    {Faker::Lorem.sentence}
+    name                     {"test"}
+    info                     {Faker::Lorem.sentence}
+    category_id              {2}
+    sales_status_id          {2}
+    shipping_free_status_id  {2}
+    prefecture_id            {2}
+    scheduled_delivery_id    {2}
+    price                    {10000}
+    association :user
   end
 end
